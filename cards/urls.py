6f329bc8bbc8780cab415c87d4c5502ed388aeb6
daniel-rawana <daniel_rawana@outlook.com>
2023-03-15 19:35:10 -0400

    Created template, route and view for updating existing cards

--- cards/urls.py
+++ cards/urls.py
@@ -13,4 +13,9 @@ urlpatterns = [
         views.CardCreateView.as_view(),
         name="card-create"
     ),
+    path(
+        "edit/<int:pk>",
+        views.CardUpdateView.as_view(),
+        name="card-update"
+    )
 ]
--- cards/views.py
+++ cards/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
     CreateView,
+    UpdateView,
 )
 
 from .models import Card
@@ -17,3 +18,6 @@ class CardCreateView(CreateView):
     model = Card
     fields = ["question", "answer", "box"]
     success_url = reverse_lazy("card-create")
+
+class CardUpdateView(CardCreateView, UpdateView):
+    success_url = reverse_lazy("card-list")
